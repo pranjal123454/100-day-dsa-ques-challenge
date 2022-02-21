ad1f0979e1c09d56175332e00bd8cc4280b4495b
pranjal123454 <68176914+pranjal123454@users.noreply.github.com>
2022-02-21 06:31:04 +0530

    Add files via upload
    https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1

---
+++ string/Print all Permutation of a string using STL.cpp
@@ -0,0 +1,48 @@
+/*Given a string S. The task is to print all permutations of a given string in lexicographically sorted order.
+
+ 
+
+Example 1:
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6 
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+Example 2:
+
+Input: ABSG
+Output:
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS 
+BASG BGAS BGSA BSAG BSGA GABS GASB 
+GBAS GBSA GSAB GSBA SABG SAGB SBAG 
+SBGA SGAB SGBA
+Explanation:
+Given string ABSG has 24 permutations.*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+
+
+
+using namespace std;
+
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t--){
+       string str;
+       cin>>str;
+       sort(str.begin(),str.end());
+       cout<<str<<"";
+       while(next_permutation(str.begin(),str.end())){
+           cout<<str<<" ";
+       }
+        cout<<endl;
+    }
+
+    return 0;
+}
